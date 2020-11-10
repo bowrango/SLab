57d170c8a331008ed7d3b815042534e779d041a0
bowrango <mjb1085@wildcats.unh.edu>
2020-11-10 18:42:50 -0500

    Section 3

--- Lab04/main.m
+++ Lab04/main.m
@@ -57,6 +57,107 @@ den = [1/omega_n1^2 2*zeta1/omega_n1 1];
 tf = tf(num, den);
 bode(tf)
 
+%% 3. Vibration Analysis 
+
+data=readmatrix('3.3');
+t=data(6:end, 3);
+acc = data(6:end, 1);
+lvt = data(6:end, 2);
+LVTslope=(lvt(60474)-lvt(56360))/(t(60474)-t(56360));
+mass = 75.6/28.35; % [ozm]
+g=32.17405*12; %in/sec^2
+lvt_sensitivity = LVTslope / g; % [mV / (in./s)]
+core_velocity = lvt  / lvt_sensitivity; % [in./s]
+vdispl=(602.2-525); %mV
+forcesense=g*mass;
+
+
+accsens=-g/acc(44585); %mV/in/sec^2
+
+% == Part a. ==
+
+%  [peak_idx, peak_mag] = peakfinder(lvt, .25);
+% [peak_idx1, peak_mag1] = peakfinder(lvt, .25,500,-1);
+% [peak_idx2, peak_mag2] = peakfinder(acc, .5,100);
+% [peak_idx3, peak_mag3] = peakfinder(acc, .1,500,-1);
+
+ figure(3)
+yyaxis left
+plot(t,lvt)
+hold on
+plot(t(53914),lvt(53914),'o')
+plot(t(45400),lvt(45400),'o')
+
+ylabel('LVT (Velocity in/sec)')
+hold off
+yyaxis right
+plot(t, acc)
+ hold on
+%plot(t(),acc(),'o')
+%plot(t(),acc(),'o')
+ylabel('Accelerometer in/sec^2)')
+xlabel('Time (s)')
+
+% == Part b. == 
+
+new_acc=acc*accsens;
+new_acc=detrend(new_acc);
+v=cumtrapz(t(41274:end),new_acc(41274:end));
+
+figure(4)
+plot(t(41274:end),v,'b'); hold on
+plot(t(41274:end),core_velocity(41274:end),'r')
+xlabel('Time (s)'); ylabel('in/sec.')
+legend('Integrated Signal', 'Core Velocity')
+
+% == Part d. == 
+
+ac=abs(core_velocity);
+b=max(ac);
+
+% == Part e. == 
+
+data1=readmatrix('3.4');
+
+t1=data1(6:end, 3);
+force = data1(6:end, 2);
+lvt1 = data1(6:end, 1);
+ 
+figure(5)
+yyaxis left
+plot(t1,lvt1)
+hold on
+
+ylabel('LVT (mV)')
+hold off
+yyaxis right
+plot(t1, force)
+hold on
+ylabel('Force Sensor mV)')
+xlabel('Time (s)')
+
+figure(6)
+yyaxis left
+plot(t1,lvt1/lvt_sensitivity)
+hold on
+
+ylabel('LVT (in/sec)')
+hold off
+yyaxis right
+plot(t1, force/500)
+hold on
+ylabel('Force Sensor lbf)')
+xlabel('Time (s)')
+
+core_velocity1 = lvt1  / lvt_sensitivity; % [in./s]
+ac1=abs(core_velocity1);
+b=max(ac1);
+%@index 40716 v=max
+forcemaxv=force(40716)/500; %lbf
+maxforce=max(abs(force))/500; %lbf
+ssf=force(83856)/500; %lbf
+massofcore=ssf/32.174049;
+
 
 
 
