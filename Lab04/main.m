d9b832b191fbfca4da37397acf704b98fcf2c4f6
bowrango <mjb1085@wildcats.unh.edu>
2020-11-10 13:53:51 -0500

    Fixed parameter values

--- Lab04/main.m
+++ Lab04/main.m
@@ -4,15 +4,14 @@ clear all; close all;
 
 % == Part a. ==
 
-weights1 = ([10.9 60.9 110.9 160.9 200.9 215.9]/1000)*9.8 * 3.60; % N -> ozf
-e0_weights1 = [619.2 705.7 788 873.4 940.6 965.4]/1000; % mV -> V
+weights1 = ([10.9 60.9 110.9 160.9 200.9 215.9]/1000)*9.8 * 3.60; % ozf
+e0_weights1 = [619.2 705.7 788 873.4 940.6 965.4]/1000; % V
 
 figure(1)
 plot(weights1, e0_weights1); grid on
 xlabel('Weight (oz_{f})'); ylabel('e_{0} (V)')
 
-% slope of the line (V/ozf)
-sensitivity = (e0_weights1(end) - e0_weights1(1)) / (weights1(end) - weights1(1));
+sensitivityA = (e0_weights1(end) - e0_weights1(1)) / (weights1(end) - weights1(1)); % V/ozf
 
 % == Part b. ==
 
@@ -27,18 +26,37 @@ t1 = linspace(-0.6, 0.6, length(e01));
 figure(2)
 plot(t1, e01); grid on
 
-% points taken from plot
+% effective mass 
+eff_mass = ((0.6022 - 0.5250) / sensitivityA); % ozm
+
+% damping ratio
 overshoot = abs(0.519 - 0.594) / 0.594;
 zeta1 = 0.56;
 
-% overshoot is about half of a full oscillation
+% natural frequency 
 period1 = t1(54034) * 2;
 omega_damped1 = 2*pi/period1;
+omega_n1 = omega_damped1 / sqrt(1 - zeta1^2); % rad/s
 
-% natural frequency 
-omega_undamped1 = omega_damped1 / sqrt(1 - zeta1^2);
+% spring constant
+eff_mass_slug = eff_mass * (1/32.174) * (1/16); % lbf-s^2/ft
+k = eff_mass_slug * (16) * (1/12) * omega_n1^2; % ozf/in.
 
-% this is kinda sketchy, not sure if it is correct.
+% == Part c. ==
+
+sensitivityB = sensitivityA * (1/32.174) * (1/12) * eff_mass; % V/in/s^2
+
+% == Part d. ==
+
+max_accel = 1.01 / sensitivityB; % in/s^2
+
+% == Part e. ==
+num = 1;
+den = [1/omega_n1^2 2*zeta1/omega_n1 1];
+
+tf = tf(num, den);
+bode(tf)
+
 
 
 
